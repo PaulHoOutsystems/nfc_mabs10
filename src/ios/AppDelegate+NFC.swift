42e75172e9adb5d0b607fd2f6c0f77f3024f5165
Paul Ho <paul.ho@outsystems.com>
2024-11-16 23:54:41 +0800

    .

--- src/ios/AppDelegate+NFC.swift
+++ src/ios/AppDelegate+NFC.swift
@@ -13,41 +13,6 @@ extension AppDelegate {
         
         NSLog("Extending UIApplicationDelegate")
         
-        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb else {
-            return false
-        }
-        
-        // Confirm that the NSUserActivity object contains a valid NDEF message.
-        if #available(iOS 12.0, *) {
-            let ndefMessage = userActivity.ndefMessagePayload
-            guard ndefMessage.records.count > 0,
-            ndefMessage.records[0].typeNameFormat != .empty else {
-                return false
-            }
-            
-            // RT3079764 NOTE: The below is commented out as it is not supported in swift 5
-
-            //let nfcPluginInstance: NfcPlugin = self.viewController.getCommandInstance("NfcPlugin") as! NfcPlugin            
-            var resolved: Bool = false;
-            // NSLog(nfcPluginInstance.debugDescription);
-            //     DispatchQueue.global().async {
-            //         let waitingTimeInterval: Double = 0.1;
-            //         print("<NFC> Did start timeout")
-            //         for _ in 1...2000 { // 5?s timeout
-            //             if ( !nfcPluginInstance.isListeningNDEF ) {
-            //             Thread.sleep(forTimeInterval: waitingTimeInterval)
-            //             } else {
-            //                 let jsonDictionary = ndefMessage.ndefMessageToJSON()
-            //                 nfcPluginInstance.sendThroughChannel(jsonDictionary: jsonDictionary)
-            //                 resolved = true
-            //                 return
-            //             }
-            //         }
-            // }
-            return resolved
-            
-        } else {
-            return false
-        }
+        return true;
     }
 }
